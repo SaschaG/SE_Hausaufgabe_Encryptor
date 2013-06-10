b73d3e51d24714013d1befe9ca7f09ca8e55d2f6
Sascha G <sushi.genji@hotmail.de>
2013-06-10 22:33:10 +0200

    fixed the Layout and made the encript function usable

--- src/GUI.java
+++ src/GUI.java
@@ -1,45 +1,66 @@
 import java.awt.Color;
+import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.border.TitledBorder;
 
 public class GUI extends JFrame {
 
 	Encryptor encryptor;
+	JTextArea textAreaOriginal;
+	JTextArea textAreaEncrypted;
 	
 	public GUI() {
 		super("Super mega Encryptor.");
 		setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
 		setBackground(Color.white);
+		
+		JPanel boxPanel = new JPanel();
+		boxPanel.setLayout(new BoxLayout(boxPanel, BoxLayout.PAGE_AXIS));
+		add(boxPanel);
+		
+		
 		Box box = Box.createVerticalBox();
 		box.setSize(400, 200);
-		add(box);
+		boxPanel.add(box);
+		//Lay out the label and scroll pane from top to bottom.
+		
+
 		
 		Box box2 = Box.createVerticalBox();
 		box2.setSize(400, 200);
-		add(box2);
+		boxPanel.add(box2);
 		
-		JTextArea textAreaOriginal = new JTextArea(15,60);
+		textAreaOriginal = new JTextArea(15,60);
 		box.add(textAreaOriginal);
-		JTextArea textAreaEncrypted = new JTextArea(15,60);
+		textAreaEncrypted = new JTextArea(15,60);
 		box.add(textAreaEncrypted);
 		
 		textAreaOriginal.setBorder(new TitledBorder("Original"));
 		textAreaEncrypted.setBorder(new TitledBorder("Encrypted"));
 		
+		
+		JPanel buttonPanel = new JPanel();
+		buttonPanel .setLayout(new BoxLayout(buttonPanel , BoxLayout.LINE_AXIS));
+		buttonPanel .setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
+		add(buttonPanel);
+		
 		JButton encryptButton = new JButton("Encrypt");
-		add(encryptButton);
+		buttonPanel.add(encryptButton);
 		EncryptPressed encryptPressed = new EncryptPressed();
 		encryptButton.addActionListener(encryptPressed);
 		
 		JButton decryptButton = new JButton("Decrypt");
-		add(decryptButton);
+		buttonPanel.add(decryptButton);
 		
 		manageEncryptor();
 	}
@@ -48,7 +69,8 @@ public class GUI extends JFrame {
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			// TODO Auto-generated method stub
-			System.out.println(encryptor.encrypt("test test test"));
+			String startText = new String(textAreaOriginal.getText());
+			textAreaEncrypted.setText(encryptor.encrypt(startText));
 		} 
 	}
 	
